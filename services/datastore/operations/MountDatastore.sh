1b1c73c1e53360d8afd49b18c24ea6d2a2834141
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-11 20:02:58 +0100

    Update MountDatastore.sh

--- services/datastore/operations/MountDatastore.sh
+++ services/datastore/operations/MountDatastore.sh
@@ -61,7 +61,7 @@ then
 elif ( [ "${bucket_type}" = "wireguard-rp-ssl" ] )
 then
         website_subdomain="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{print $1}'`"
-        WEBSITE_URL="`/bin/echo ${WEBSITE_URL} | /bin/sed "s/${website_subdomain}-protected/${website_subdomain}/g"`"
+        WEBSITE_URL="`/bin/echo ${WEBSITE_URL} | /bin/sed "s/\-protected//g"`"
         if ( [ "${SSL_GENERATION_SERVICE}" = "LETSENCRYPT" ] )
         then
                 service_token="lets"
