27d62dd270c8192afadd76fe7514b7f73893d87e
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-22 23:20:20 +0100

    Update ListDatastore.sh

--- services/datastore/operations/ListDatastore.sh
+++ services/datastore/operations/ListDatastore.sh
@@ -21,7 +21,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #########################################################################################
 #########################################################################################
-#set -x
+set -x
 
 bucket_type="${1}"
 additional_specifier="${2}"
