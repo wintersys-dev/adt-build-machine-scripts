ddfbcae9fc01b3b428216bd20365e8220867e737
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-27 10:15:39 +0100

    Update PerformDeleteFromDatastore.sh

--- services/datastore/operations/PerformDeleteFromDatastore.sh
+++ services/datastore/operations/PerformDeleteFromDatastore.sh
@@ -18,7 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ######################################################################################
 ######################################################################################
-set -x
+#set -x
 
 file_to_delete="${1}"
 count="${2}"
@@ -47,11 +47,15 @@ elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
 then
         host_base="`/bin/grep ^host_base /root/.s5cfg-${count} | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`"
         datastore_cmd="/usr/bin/s5cmd --credentials-file /root/.s5cfg-${count} --endpoint-url https://${host_base} rm s3://"
-        wildcard="*"
+        if ( [ "`/bin/echo ${file_to_delete} | /bin/grep 'root$'`" != "" ] )
+        then
+                file_to_delete="`/bin/echo ${file_to_delete} | /bin/sed "s/root$//g"`"
+                wildcard="*"
+        fi
 elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
 then
         host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-${count} | /bin/grep "^endpoint" | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-       
+
         include=""
         if ( [ "${file_to_delete}" != "" ] && [ "`/bin/echo ${file_to_delete} | /bin/grep '*'`" != "" ] )
         then
