feb8e37fe56fd02e24fef5455e2cc17cd5d4cfb0
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-05-28 11:56:29 +0100

    Update ListDatastore.sh

--- services/datastore/operations/ListDatastore.sh
+++ services/datastore/operations/ListDatastore.sh
@@ -57,20 +57,6 @@ then
         fi
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
         active_bucket="${active_bucket}-${DNS_CHOICE}-${service_token}-ssl"
-elif ( [ "${bucket_type}" = "wireguard-rp-ssl" ] )
-then
-        website_subdomain="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{print $1}'`"
-        WEBSITE_URL="`/bin/echo ${WEBSITE_URL} | /bin/sed "s/\-protected//g"`"
-
-        if ( [ "${SSL_GENERATION_SERVICE}" = "LETSENCRYPT" ] )
-        then
-                service_token="lets"
-        elif ( [ "${SSL_GENERATION_SERVICE}" = "ZEROSSL" ] )
-        then
-                service_token="zero"
-        fi
-        active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
-        active_bucket="${active_bucket}-${DNS_CHOICE}-${service_token}-ssl"
 elif ( [ "${bucket_type}" = "multi-region" ] )
 then
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-multi-region"
