b4b9c7e9d49aa5c922147c8bdd7bab80545500d6
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-06-29 09:40:35 +0100

    Fix variable name for NO_REVERSE_PROXY to NO_REVERSE_PROXIES

--- services/security/firewall/vultr/ConfigureNativeFirewall.sh
+++ services/security/firewall/vultr/ConfigureNativeFirewall.sh
@@ -143,7 +143,7 @@ then
                 /usr/bin/vultr firewall rule create ${firewall_id} --protocol=tcp --port=443 --size=32 --ip-type=v4  --source=cloudflare --subnet=10.0.0.0/8
                 /usr/bin/vultr firewall rule create ${firewall_id} --protocol=icmp --size=32 --ip-type=v4 --subnet=0.0.0.0/0
         fi
-elif ( ( [ "${NO_REVERSE_PROXY}" = "0" ] && [ "${firewall_name}" = "adt-webserver" ] ) || [ "${firewall_name}" = "adt-reverseproxy" ] || [ "${firewall_name}" = "adt-authenticator" ] )
+elif ( ( [ "${NO_REVERSE_PROXIES}" = "0" ] && [ "${firewall_name}" = "adt-webserver" ] ) || [ "${firewall_name}" = "adt-reverseproxy" ] || [ "${firewall_name}" = "adt-authenticator" ] )
 then
                 /usr/bin/vultr firewall rule create ${firewall_id} --protocol=tcp --port=443 --size=32 --ip-type=v4 --subnet=0.0.0.0/0
 fi
