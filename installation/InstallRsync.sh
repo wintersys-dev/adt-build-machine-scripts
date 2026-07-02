e0602008ea50e6043299f6cb9f7189f4ec7ddbdd
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 18:24:05 +0100

    Update InstallRsync.sh

--- installation/InstallRsync.sh
+++ installation/InstallRsync.sh
@@ -51,11 +51,11 @@ if ( [ "${manager}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		eval ${install_command} rsync
+		eval ${install_command} rsync ${tail_options}
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		eval ${install_command} rsync
+		eval ${install_command} rsync ${tail_options}
 	fi
 fi
