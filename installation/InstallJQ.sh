c92940d71dba7fa16cb5d0ea56bb553c54392a20
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 18:22:33 +0100

    Enhance jq installation with tail options
    Added tail options to jq installation command for better handling.

--- installation/InstallJQ.sh
+++ installation/InstallJQ.sh
@@ -51,11 +51,11 @@ if ( [ "${manager}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		eval ${install_command} jq
+		eval ${install_command} jq ${tail_options}
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		eval ${install_command} jq	
+		eval ${install_command} jq ${tail_options}
 	fi
 fi
