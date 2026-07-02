1339a439e7011ee0a88fb36522b380db9b0a7ebd
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-02 18:24:23 +0100

    Update InstallRuby.sh

--- installation/InstallRuby.sh
+++ installation/InstallRuby.sh
@@ -51,12 +51,12 @@ if ( [ "${manager}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		eval ${install_command} ruby
+		eval ${install_command} ruby ${tail_options}
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		eval ${install_command} ruby
+		eval ${install_command} ruby ${tail_options}
 	fi
 fi
 
