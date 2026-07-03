dc6f08a0c53b0daa66710747aa5cb9a7df447c20
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-03 08:09:22 +0100

    Update RebootInfrastructure.sh

--- helpers/servers/RebootInfrastructure.sh
+++ helpers/servers/RebootInfrastructure.sh
@@ -120,7 +120,7 @@ fi
 
 SERVER_USERNAME="`/bin/cat ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER`"
 SERVER_USER_PASSWORD="`/bin/cat ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSERPASSWORD`"
-SUDO="DEBIAN_FRONTEND=noninteractive /bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S -E "
+SUDO="DEBIAN_FRONTEND=noninteractive /bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S "
 SSH_PORT="`${BUILD_HOME}/helpers/services/GetVariableValue.sh SSH_PORT`"
 
 /bin/echo "OK, rebooting your infrastructure"
