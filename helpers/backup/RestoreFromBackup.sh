56e8e6f2594a222d64438f3a11429ac71da8bfff
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-07 10:38:55 +0100

    Update RestoreFromBackup.sh

--- helpers/backup/RestoreFromBackup.sh
+++ helpers/backup/RestoreFromBackup.sh
@@ -9,4 +9,16 @@ else
         restoration_no="`/usr/bin/expr ${existing_restoration_no} + 1`"
 fi
 
-/bin/grep -rlZ "${DB_NAME}" ${BUILD_HOME}/runtime | /usr/bin/xargs -0 /bin/sed -i "s/${DB_NAME}.*/${DB_NAME}_restored-${restoration_no}/g
+/bin/grep -rlZ "${DB_NAME}" ${BUILD_HOME}/runtime | /usr/bin/xargs -0 /bin/sed -i "s/${DB_NAME}.*/${DB_NAME}_restored-${restoration_no}/g"
+
+command='/bin/grep -rlZ "'${DB_NAME}'" '${HOME}' | /usr/bin/xargs -0 /bin/sed -i "s/'${DB_NAME}'.*/'${DB_NAME}'_restored-${restoration_no}/g"'
+${HOME}/utilities/remote/ConnectToDatabaseVPS.sh "${SUDO} ${command}"
+command='/bin/grep -rlZ "'${DB_NAME}'" '/var/www/html' | /usr/bin/xargs -0 /bin/sed -i "s/'${DB_NAME}'.*/'${DB_NAME}'_restored-${restoration_no}/g"'
+${HOME}/utilities/remote/ConnectToDatabaseVPS.sh "${SUDO} ${command}"
+
+${HOME}/utilities/remote/ConnectToDatabaseVPS.sh "${SUDO} ${HOME}/application/InstallApplication.sh"
+${HOME}/utilities/remote/ConnectToDatabaseVPS.sh "${SUDO} ${HOME}/application/configuration/InitialiseApplicationConfiguration.sh"
+
+command='/bin/grep -rlZ "'${DB_NAME}'" '${HOME}' | /usr/bin/xargs -0 /bin/sed -i "s/'${DB_NAME}'.*/'${DB_NAME}'_restored-${restoration_no}/g"'
+${HOME}/utilities/remote/ConnectToDatabaseVPS.sh "${SUDO} ${command}"
+${HOME}/utilities/remote/ConnectToDatabaseVPS.sh "${SUDO} ${HOME}/application/db/InstallApplicationDB.sh"
