557892165887d3373400828d7b82e7ab51c91425
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-25 22:29:15 +0100

    Update CopyFromWebserver.sh

--- helpers/servers/CopyFromWebserver.sh
+++ helpers/servers/CopyFromWebserver.sh
@@ -109,17 +109,21 @@ then
 fi
 
 /bin/echo "Which webserver would you like to connect to?"
-count=1
+count="1"
+ip_select="0"
 for ip in ${ips}
 do
-	/bin/echo "${count}:   ${ip}"
-	/bin/echo "Press Y/N to connect..."
-	read response
-
-	if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
+	if ( [ "${ip_select}" = "0" ] )
 	then
-		WEBSERVER_IP=${ip}
-		break
+		/bin/echo "${count}:   ${ip}"
+		/bin/echo "Press Y/N to connect..."
+		read response
+
+		if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
+		then
+			WEBSERVER_IP=${ip}
+			ip_select="1"
+		fi
 	fi
 	count="`/usr/bin/expr ${count} + 1`"
 done
