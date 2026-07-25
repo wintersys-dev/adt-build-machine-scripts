5eafcf48ff264980443ecf584f9bbe180183d5d3
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-25 19:16:21 +0100

    Refactor autoscaler connection prompts in script

--- helpers/servers/CopyToAutoscaler.sh
+++ helpers/servers/CopyToAutoscaler.sh
@@ -113,26 +113,45 @@ then
 	exit
 fi
 
-
-/bin/echo "Which autoscaler would you like to connect to?"
-count=1
-for ip in ${ips}
-do
-	/bin/echo "${count}:   ${ip}"
-	/bin/echo "Press Y/N to connect..."
-	read response
-
-	if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
-	then
-		AS_IP=${ip}
-		break
-	fi
-	count="`/usr/bin/expr ${count} + 1`"
-done
-
-if ( [ "${response}" = "N" ] )
+copy_to_all="0"
+ip_selected="0"
+response="N"
+response1="N"
+if ( [ "`/bin/echo ${ips} | /usr/bin/wc -l`" = "1" ] )
 then
-	exit
+        AS_IP="${ips}"
+else
+        /bin/echo "Do you want to copy your file to all your autoscaler  machines? (Y|y)"
+        read response
+        if ( [ "${response}" = "y" ] || [ "${response}" = "Y" ] )
+        then
+                copy_to_all="1"
+        else
+                /bin/echo "OK, which autoscaler would you like to copy your file to?"
+                count=1
+                for ip in ${ips}
+                do
+                        if ( [ "${ip_selected}" = "0" ] )
+                        then
+                                /bin/echo "${count}:   ${ip}"
+                                /bin/echo "Press Y/N to connect..."
+                                read response1
+
+                                if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
+                                then
+                                        AS_IP=${ip}
+                                        ip_selected="1"
+                                fi
+                                count="`/usr/bin/expr ${count} + 1`"
+                        fi
+                done
+
+        fi
+fi
+
+if ( [ "${response1}" = "N" ] )
+then
+        exit
 fi
 
 SERVER_USER="`/bin/cat ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER`"
