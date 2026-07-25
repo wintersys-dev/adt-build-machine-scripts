066eeca4f63b04a6965035668c18c082f37fea8c
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-25 22:25:59 +0100

    Improve authenticator selection logic
    Refactor user input handling for authenticator selection.

--- helpers/servers/CopyFromAuthenticator.sh
+++ helpers/servers/CopyFromAuthenticator.sh
@@ -97,17 +97,21 @@ then
 fi
 
 /bin/echo "Which authenticator would you like to connect to?"
-count=1
+count="1"
+ip_select="0"
 for ip in ${ips}
 do
-        /bin/echo "${count}:   ${ip}"
-        /bin/echo "Press Y/N to connect..."
-        read response
+	if ( [ "${ip_select}" = "0" ] )
+	then
+                /bin/echo "${count}:   ${ip}"
+                /bin/echo "Press Y/N to connect..."
+                read response
 
-        if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
-        then
-                AUTHENTICATOR_IP=${ip}
-                break
+                if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
+                then
+                        AUTHENTICATOR_IP=${ip}
+                        ip_select="1"
+                fi
         fi
         count="`/usr/bin/expr ${count} + 1`"
 done
