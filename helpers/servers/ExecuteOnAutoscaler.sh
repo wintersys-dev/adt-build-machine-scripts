571889e8c66586d6d79ae161e8e6b989820f0a89
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-25 22:12:41 +0100

    Update ExecuteOnAutoscaler.sh

--- helpers/servers/ExecuteOnAutoscaler.sh
+++ helpers/servers/ExecuteOnAutoscaler.sh
@@ -178,5 +178,13 @@ then
 		/usr/bin/ssh-keyscan  -p ${SSH_PORT} ${AUTOSCALER_IP} > ${AUTOSCALER_PUBLIC_KEYS}
 	fi
 
-	/usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${AUTOSCALER_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USERNAME}@${AUTOSCALER_IP} "${command}"
+	if ( [ "${execute_on_all}" = "1" ] )
+	then
+		for WEBSERVER_IP in ${ips}
+		do
+			/usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${AUTOSCALER_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USERNAME}@${AUTOSCALER_IP} "${command}"
+		done
+	else
+		/usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${AUTOSCALER_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USERNAME}@${AUTOSCALER_IP} "${command}"
+    fi
 fi
