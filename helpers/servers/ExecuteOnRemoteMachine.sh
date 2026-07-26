b2e3eeac5628ddb578a90a916ee00b26a920dab0
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-26 22:07:44 +0100

    Add password handling for remote machine execution

--- helpers/servers/ExecuteOnRemoteMachine.sh
+++ helpers/servers/ExecuteOnRemoteMachine.sh
@@ -189,6 +189,8 @@ fi
 for MACHINE_IP in ${MACHINE_IPS}
 do
         SERVER_USERNAME="`/bin/cat ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER`"
+        SERVER_USER_PASSWORD="`/bin/cat ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSERPASSWORD`"
+        SUDO="DEBIAN_FRONTEND=noninteractive /bin/echo ${SERVER_USER_PASSWORD} | /usr/bin/sudo -S "
         SSH_PORT="`${BUILD_HOME}/helpers/services/GetVariableValue.sh SSH_PORT`"
         MACHINE_PUBLIC_KEYS="${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/${machine_type}_${MACHINE_IP}keys"
 
