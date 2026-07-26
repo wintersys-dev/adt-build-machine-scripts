4f8083cceb2bca30c1c18d367ebd6ebdb17c2430
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-26 10:25:21 +0100

    Rename variable ip_select to ip_selected

--- helpers/servers/ConnectToReverseProxy.sh
+++ helpers/servers/ConnectToReverseProxy.sh
@@ -91,9 +91,9 @@ fi
 
 /bin/echo "Which reverseproxy would you like to connect to?"
 count="1"
-ip_select="0"
+ip_selected="0"
 for ip in ${ips}
-do	if ( [ "${ip_select}" = "0" ] )
+do	if ( [ "${ip_selected}" = "0" ] )
 	then
 		/bin/echo "${count}:   ${ip}"
 		/bin/echo "Press Y/N to connect..."
@@ -101,7 +101,7 @@ do	if ( [ "${ip_select}" = "0" ] )
 		if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
 		then
 			REVERSEPROXY_IP=${ip}
-			ip_select="1"
+			ip_selected="1"
 		fi
 	fi
 	count="`/usr/bin/expr ${count} + 1`"
