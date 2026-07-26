2a80bf3f93054dbb846b8b8ccf3e22d0b2ab0107
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-26 10:25:50 +0100

    Update ConnectToWebserver.sh

--- helpers/servers/ConnectToWebserver.sh
+++ helpers/servers/ConnectToWebserver.sh
@@ -91,10 +91,10 @@ fi
 
 /bin/echo "Which webserver would you like to connect to?"
 count="1"
-ip_select="0"
+ip_selected="0"
 for ip in ${ips}
 do	
-	if ( [ "${ip_select}" = "0" ] )
+	if ( [ "${ip_selected}" = "0" ] )
 	then
 		/bin/echo "${count}:   ${ip}"
 		/bin/echo "Press Y/N to connect..."
@@ -103,7 +103,7 @@ do
 		if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
 		then
 			WEBSERVER_IP=${ip}
-			ip_select="1"
+			ip_selected="1"
 		fi
 	fi
 	count="`/usr/bin/expr ${count} + 1`"
