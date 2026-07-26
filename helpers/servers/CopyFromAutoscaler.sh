7f0352512a68a2db5f9df24f97a02d96e955cec8
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-26 10:26:42 +0100

    Fix variable name from ip_select to ip_selected

--- helpers/servers/CopyFromAutoscaler.sh
+++ helpers/servers/CopyFromAutoscaler.sh
@@ -98,10 +98,10 @@ fi
 
 /bin/echo "Which autoscaler would you like to connect to?"
 count="1"
-ip_select="0"
+ip_selected="0"
 for ip in ${ips}
 do
-	if ( [ "${ip_select}" = "0" ] )
+	if ( [ "${ip_selected}" = "0" ] )
 	then
 		/bin/echo "${count}:   ${ip}"
 		/bin/echo "Press Y/N to connect..."
@@ -110,7 +110,7 @@ do
 		if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
 		then
 			AUTOSCALER_IP=${ip}
-			ip_select="1"
+			ip_selected="1"
 		fi
 	fi
 	count="`/usr/bin/expr ${count} + 1`"
