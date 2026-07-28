eeabc33c29675a23afd43039b026de2a3e051544
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-07-28 10:47:16 +0100

    Update ExecuteOnRemoteMachine.sh

--- helpers/servers/ExecuteOnRemoteMachine.sh
+++ helpers/servers/ExecuteOnRemoteMachine.sh
@@ -307,6 +307,9 @@ do
         elif ( [ "${command}" = "prevent-webserver-temination" ] )
         then
                 /usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${MACHINE_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USERNAME}@${MACHINE_IP} "${SUDO} /bin/touch /home/${SERVER_USERNAME}/runtime/PREVENT_WEBSERVER_TERMINATIONS"
+        elif ( [ "${command}" = "reenable-webserver-temination" ] )
+        then
+                /usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${MACHINE_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USERNAME}@${MACHINE_IP} "${SUDO} /bin/rm /home/${SERVER_USERNAME}/runtime/PREVENT_WEBSERVER_TERMINATIONS"
         else
                 /usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${MACHINE_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USERNAME}@${MACHINE_IP} "${command}"
         fi
