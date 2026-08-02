ecda5d9f112a58ebe9e18435c777c162e12bc9cf
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-02 09:16:20 +0100

    Add installation script for ipcalc
    This script installs ipcalc based on the package manager detected from the configuration file. It supports various package managers like apt, apt-get, nala, and aptitude.

---
+++ installation/InstallIPCalc.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+######################################################################################################
+# Description: This script will install ipcalc
+# Author: Peter Winter
+# Date: 17/01/2017
+#######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+#set -x
+
+if ( [ "${1}" != "" ] )
+then
+	buildos="${1}"
+fi
+
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+
+manager=""
+options=""
+tail_options=""
+if ( [ "`/bin/grep "^PACKAGEMANAGER:*" ${BUILD_HOME}/configuration/software.dat | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
+then
+	manager="/usr/bin/apt"
+	options="-o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y"
+elif ( [ "`/bin/grep "^PACKAGEMANAGER:*" ${BUILD_HOME}/configuration/software.dat | /usr/bin/awk -F':' '{print $NF}'`" = "apt-get" ] )
+then
+	manager="/usr/bin/apt-get"
+	options="-o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y"
+elif ( [ "`/bin/grep "^PACKAGEMANAGER:*" ${BUILD_HOME}/configuration/software.dat | /usr/bin/awk -F':' '{print $NF}'`" = "nala" ] )
+then
+	manager="/usr/bin/nala"
+	tail_options="-y"
+elif ( [ "`/bin/grep "^PACKAGEMANAGER:*" ${BUILD_HOME}/configuration/software.dat | /usr/bin/awk -F':' '{print $NF}'`" = "aptitude" ] )
+then
+        manager="/usr/bin/aptitude"
+        options="-y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold'"
+fi
+
+export DEBIAN_FRONTEND=noninteractive 
+install_command="${manager} ${options} install " 
+
+if ( [ "${manager}" != "" ] )
+then
+	if ( [ "${buildos}" = "ubuntu" ] )
+	then
+		eval ${install_command} ipcalc ${tail_options}
+	fi
+
+	if ( [ "${buildos}" = "debian" ] )
+	then
+		eval ${install_command} ipcalc ${tail_options}
+	fi
+fi
