a6407c3a348e090413d45e1afa7effe27a12a7a5
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-03 08:44:52 +0100

    Update OverrideScript.sh

--- templatedconfigurations/templateoverrides/OverrideScript.sh
+++ templatedconfigurations/templateoverrides/OverrideScript.sh
@@ -72,7 +72,7 @@ PermitEmptyPasswords no
 KerberosAuthentication no
 GSSAPIAuthentication no
 UsePAM no
-X11Forwarding no" > /etc/ssh/sshd_config.d/99-hardening.conf
+X11Forwarding no" >> /etc/ssh/sshd_config.d/99-hardening.conf
 
 if ( [ "${BUILDMACHINE_SSH_PORT}" = "" ] )
 then
