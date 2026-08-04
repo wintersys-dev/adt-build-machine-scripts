81058e6d0a4fcd9580e188aeb470d453c33bf8b9
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-04 19:37:00 +0100

    Limit shutdown monitoring to 60 seconds
    Modify shutdown monitoring script to limit checks to 60 seconds.

--- helpers/services/MonitorForShutdown.sh
+++ helpers/services/MonitorForShutdown.sh
@@ -1,8 +1,9 @@
 
-
-while ( [ 1 ] )
+count="0"
+while ( [ "${count}" -lt "60" ] )
 do
   /bin/sleep 1
+  count="/usr/bin/expr ${count} + 1`"
   if ( [ -f /tmp/SHUTDOWN_INITIATED ] )
   then
       /bin/shutdown -r now
