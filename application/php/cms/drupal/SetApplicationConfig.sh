90ce905487a882b5a05b8829df6d8b55205733fd
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-15 20:14:37 +0100

    Update SetApplicationConfig.sh

--- application/php/cms/drupal/SetApplicationConfig.sh
+++ application/php/cms/drupal/SetApplicationConfig.sh
@@ -44,7 +44,6 @@ fi
 
 /bin/sed -i "s/XXXXHASH_SALTXXXX/`/usr/bin/openssl rand -base64 32 | /usr/bin/tr -cd 'a-zA-Z0-9' | /usr/bin/cut -b 1-32`/" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
 
-weboot_directory="`/bin/grep "WEBROOT_DIRECTORY" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat | /usr/bin/awk -F':' '{print $NF}'`"
-/bin/sed -i "s/XXXXWEBROOT_DIRECTORYXXXX/${webroot_directory}/g" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
-
+webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat | /usr/bin/awk -F':' '{print $NF}'`"
+/bin/sed -i "s;XXXXWEBROOT_DIRECTORYXXXX;${webroot_directory};g" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
 
