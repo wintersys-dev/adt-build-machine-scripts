f756f1b9a3ce46ae82ac83881e406525635a7bb2
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-16 16:34:52 +0100

    Add webroot_directory extraction to SetApplicationConfig.sh

--- application/php/cms/joomla/SetApplicationConfig.sh
+++ application/php/cms/joomla/SetApplicationConfig.sh
@@ -77,6 +77,7 @@ fi
 /bin/sed -i "s/XXXXSMTP_PORTXXXX/${smtp_port}/" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
 /bin/sed -i "s/XXXXSMTP_HOSTXXXX/${smtp_host}/" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
 
+webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat | /usr/bin/awk -F':' '{print $NF}'`"
 /bin/sed -i "s;XXXXWEBROOT_DIRECTORYXXXX;${webroot_directory};g" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
 
 
