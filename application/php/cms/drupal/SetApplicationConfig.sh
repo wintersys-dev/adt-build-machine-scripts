81f298fab73b853a0b52c9b59822af271d5deecb
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-02 23:56:46 +0100

    Add webroot_subdirectory handling in SetApplicationConfig.sh

--- application/php/cms/drupal/SetApplicationConfig.sh
+++ application/php/cms/drupal/SetApplicationConfig.sh
@@ -47,6 +47,17 @@ fi
 webroot_directory="`/bin/grep "^WEBROOT_DIRECTORY" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat | /usr/bin/awk -F':' '{print $NF}'`"
 /bin/sed -i "s;XXXXWEBROOT_DIRECTORYXXXX;${webroot_directory};g" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
 
+webroot_subdirectory="`/bin/grep "^WEBROOT_SUBDIRECTORY" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat | /usr/bin/awk -F':' '{print $NF}'`"
+
+if ( [ "${websroot_subdirectory}" = "" ] )
+then
+	webroot_subdirectory="web"
+fi
+
+/bin/sed -i "s;XXXXWEBROOT_SUBDIRECTORYXXXX;${webroot_subdirectory};g" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
+
+
+XXXXWEBROOT_SUBDIRECTORYXXXX
 # The application can override the system wide value for the PHP version you might want to do this if an application is behind on PHP version updates
 # and a particular application needs an older version of PHP then you want to set by default in your template/StackScript
 php_version="`/bin/grep "^PHP_VERSION" ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat | /usr/bin/awk -F':' '{print $NF}'`"
