d511650853ea7c4ee2d976457f8c56192211b131
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-10 21:43:39 +0100

    Update InitialiseAssetDatastore.sh

--- initialisation/InitialiseAssetDatastore.sh
+++ initialisation/InitialiseAssetDatastore.sh
@@ -49,4 +49,10 @@ then
                         fi
                 fi
         done
+        
+        if ( [ "`/bin/grep "^DATASTOREMOUNTTOOL:" ${BUILD_HOME}/configuration/software.dat`" = "" ] )
+        then
+                #If PERSIST_ASSETS_TO_CLOUD is enabled and no DATASTOREMOUNTTOOL is configured for use default back to the rclone:binary install
+                /bin/sed 's/#DATASTOREMOUNTTOOL:rclone:binary/DATASTOREMOUNTTOOL:rclone:binary/g' ${BUILD_HOME}/configuration/software.dat
+        fi
 fi
