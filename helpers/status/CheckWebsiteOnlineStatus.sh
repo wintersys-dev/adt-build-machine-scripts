ac0277817e888f2ce807713c8a91cea103e62e44
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-21 22:00:05 +0100

    Update CheckWebsiteOnlineStatus.sh

--- helpers/status/CheckWebsiteOnlineStatus.sh
+++ helpers/status/CheckWebsiteOnlineStatus.sh
@@ -18,7 +18,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 ###################################################################################
 ###################################################################################
-#set -x
+set -x
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 
