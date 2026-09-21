d65d88e2526a92ca6f8e2e67913b267f72f3e674
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-21 10:28:31 +0100

    Add InstallTextBrowser.sh for text browser installation
    This script installs the preferred text browser based on the configuration settings.

---
+++ installation/InstallTextBrowser.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#####################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: Install the our text browser of choice
+#####################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
+
+if ( [ "$1" != "" ] )
+then
+	buildos="${1}"
+fi
+
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+
+if ( [ "`/bin/grep "^TEXTBROWSER:*" ${BUILD_HOME}/configuration/software.dat | /bin/grep lynx`" != "" ] )
+then
+	${BUILD_HOME}/installation/InstallLynx.sh ${buildos}
+elif ( [ "`/bin/grep "^TEXTBROWSER:*" ${BUILD_HOME}/configuration/software.dat | /bin/grep w3m`" != "" ] )
+then
+	${BUILD_HOME}/installation/InstallW3M.sh ${buildos}
+fi
