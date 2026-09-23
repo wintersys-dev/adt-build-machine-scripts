5dbd93e622efdc9cb45904f9b34deebc5a442f6c
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-23 21:54:05 +0100

    Create AdjustScaling.sh

---
+++ helpers/scaling/AdjustScaling.sh
@@ -0,0 +1,14 @@
+if ( [ ! -f  ./AdjustScaling.sh ] )
+then
+	/bin/echo "Sorry, this script has to be run from the ${BUILD_HOME}/helpers/services subdirectory"
+	exit
+fi
+
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+
+if ( [ "`${BUILD_HOME}/helpers/services/GetVariableValue.sh DEPLOYMENT_MODE`" != "PRODUCTION" ] )
+then
+	/bin/echo "You are not in PRODUCTION mode, cannot set scaling parameters"
+	exit
+fi
+
