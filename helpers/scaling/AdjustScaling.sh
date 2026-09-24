a2d35b9dea1b086ba35f0a74e3df1f82d25f9199
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 09:03:49 +0100

    Update AdjustScaling.sh

--- helpers/scaling/AdjustScaling.sh
+++ helpers/scaling/AdjustScaling.sh
@@ -1,48 +1,59 @@
+#!/bin/sh
+
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 CLOUDHOST="`${BUILD_HOME}/helpers/services/GetVariableValue.sh CLOUDHOST`"
 BUILD_IDENTIFIER="`${BUILD_HOME}/helpers/services/GetVariableValue.sh BUILD_IDENTIFIER`"
 REGION="`${BUILD_HOME}/helpers/services/GetVariableValue.sh REGION`"
 
+MAX_WEBSERVERS="20"
 
 sourcefile="${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/scaling.conf"
+authorised_to_scale_file="${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/authorised_to_scale.conf"
+
+machine_type="autoscaler"
+machine_type_token="as"
 
 if ( [ ! -f  ./AdjustScaling.sh ] )
 then
-	/bin/echo "Sorry, this script has to be run from the ${BUILD_HOME}/helpers/services subdirectory"
-	exit
+        /bin/echo "Sorry, this script has to be run from the ${BUILD_HOME}/helpers/services subdirectory"
+        exit
 fi
 
-BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+if ( [ ! -f ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling ] )
+then
+        /bin/mkdir -p ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling
+fi
 
 if ( [ "`${BUILD_HOME}/helpers/services/GetVariableValue.sh DEPLOYMENT_MODE`" != "PRODUCTION" ] )
 then
-	/bin/echo "You are not in PRODUCTION mode, cannot set scaling parameters"
-	exit
+        /bin/echo "You are not in PRODUCTION mode, cannot set scaling parameters"
+        exit
 fi
 
 /bin/echo "Please enter the number of webservers you want to be provisioned and active"
 read no_webservers
 
 case "${no_webservers}" in
-    # Check if empty or contains anything other than digits
-    ''|*[!0-9]*) 
+        # Check if empty or contains anything other than digits
+        ''|*[!0-9]*) 
         /bin/echo "Error: Number of webservers must be a positive integer." 
-		;;
-    *) 
+        exit
+        ;;
+*) 
         # Check if the no_webservers is within the 1-${MAX_WEBSERVERS} range
         if ( [ "${no_webservers}" -ge "1" ] && [ "${no_webservers}" -le "${MAX_WEBSERVERS}" ] )
-		then
-            /bin/echo "Valid number of webservers set: ${no_webservers}"
+        then
+                /bin/echo "Valid number of webservers set: ${no_webservers}"
         else
-            /bin/echo "Error: number of webservers must be between 1 and ${MAX_WEBSERVERS}"
-			exit
+                /bin/echo "Error: number of webservers must be between 1 and ${MAX_WEBSERVERS}"
+                exit
         fi
         ;;
 esac
 
 if ( [ -f ${sourcefile} ] )
 then
-	/bin/rm ${sourcefile}
+        /bin/rm ${sourcefile}
 fi
 
 start_index="1"
@@ -57,24 +68,49 @@ count="0"
 
 while ( [ "${count}" -lt "${no_autoscalers}" ] )
 do
-    if ( [ "${count}" -lt "${remainder}" ] )
-    then
-        current_size="`/usr/bin/expr ${base} + 1`"
-    else
-        current_size="${base}"
-    fi
+        if ( [ "${count}" -lt "${remainder}" ] )
+        then
+                current_size="`/usr/bin/expr ${base} + 1`"
+        else
+                current_size="${base}"
+        fi
 
-    if ( [ "${current_size}" -gt "0" ] )
-    then
-        current_end="`/usr/bin/expr ${current_start} + ${current_size}`"
-        /bin/echo "Autoscaler `/usr/bin/expr ${count} + 1` is responsible for provisioning `/usr/bin/expr ${current_end} - ${current_start}` webservers" >> ${sourcefile}
-        current_start="`/usr/bin/expr ${current_end} + 1`"
-    fi
+        if ( [ "${current_size}" -gt "0" ] )
+        then
+                current_end="`/usr/bin/expr ${current_start} + ${current_size}`"
+                /bin/echo "Autoscaler `/usr/bin/expr ${count} + 1` is responsible for provisioning `/usr/bin/expr ${current_end} - ${current_start}` webservers" >> ${sourcefile}
+                current_start="`/usr/bin/expr ${current_end} + 1`"
+        fi
 
-    count="`/usr/bin/expr ${count} + 1`"
+        count="`/usr/bin/expr ${count} + 1`"
 done
 
+token_to_match="${machine_type_token}-`${BUILD_HOME}/helpers/services/GetVariableValue.sh REGION`-${BUILD_IDENTIFIER}"
 
+if ( [ -f ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
+then
+        ips="`${BUILD_HOME}/services/server/GetServerPrivateIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
+else
+        ips="`${BUILD_HOME}/services/server/GetServerIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
+fi
 
-${BUILD_HOME}/helpers/scaling/TestIfScalingAllowed.sh
+/bin/cp /dev/null ${authorised_to_scale_file}
+
+for ip in ${ips}
+do
+        if ( [ "`${BUILD_HOME}/helpers/scaling/TestIfScalingAllowed.sh ${ip}`" = "SCALING_ENABLED" ] )
+        then
+                /bin/echo "${ip}" >> ${authorised_to_scale_file}
+        fi
+done
+
+if ( [ "`/usr/bin/wc -l ${authorised_to_scale_file}`" = "${no_autoscalers}" ] )
+then
+        for ip in `/bin/cat ${authorised_to_scale_file}`
+        do
+                ${BUILD_HOME}/helpers/scaling/UpdateAutoscalersWithScalingConfig.sh ${ip}
+        done
+else
+        /bin/echo "Not all your autoscalers are allowing scaling events right now"
+fi
 
