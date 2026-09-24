21b949b494a52adcb069fcc8f60bb301ce3d9813
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 09:05:04 +0100

    Fix syntax errors and improve SSH key handling

--- helpers/scaling/TestIfScalingAllowed.sh
+++ helpers/scaling/TestIfScalingAllowed.sh
@@ -1,4 +1,6 @@
-set -x
+#!/bin/sh
+
+#set -x
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 SSH_PORT="`${BUILD_HOME}/helpers/services/GetVariableValue.sh SSH_PORT`"
@@ -11,43 +13,28 @@ SERVER_USER="`/bin/cat ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/cr
 machine_type="autoscaler"
 machine_type_token="as"
 
+ip="${1}"
+
 if ( [ ! -f ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/scaling.conf ] )
 then
         exit
 else
         sourcefile="${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/scaling.conf"
-        /bin/cp ${sourcefile} ${sourcefile)-incoming
+        /bin/cp ${sourcefile} ${sourcefile}-incoming
 fi
 
-token_to_match="${machine_type_token}-`${BUILD_HOME}/helpers/services/GetVariableValue.sh REGION`-${BUILD_IDENTIFIER}"
+MACHINE_PUBLIC_KEYS="${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/${machine_type}_${ip}keys"
 
-if ( [ -f ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
+if ( [ ! -f ${MACHINE_PUBLIC_KEYS} ] )
 then
-        ips="`${BUILD_HOME}/services/server/GetServerPrivateIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
-else
-        ips="`${BUILD_HOME}/services/server/GetServerIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
+        /usr/bin/ssh-keyscan  -p ${SSH_PORT} ${ip} > ${MACHINE_PUBLIC_KEYS}
 fi
 
+if ( [ "`/bin/cat ${MACHINE_PUBLIC_KEYS}`" = "" ] )
+then
+        /bin/echo "Couldn't initiate ssh key scan please try again (make sure the machine is online"
+        /bin/rm ${MACHINE_PUBLIC_KEYS}
+        exit
+fi
 
-for ip in ${ips}
-do
-        if ( [ ! -f ${MACHINE_PUBLIC_KEYS} ] )
-        then
-                /usr/bin/ssh-keyscan  -p ${SSH_PORT} ${ip} > ${MACHINE_PUBLIC_KEYS}
-        fi
-
-        if ( [ "`/bin/cat ${MACHINE_PUBLIC_KEYS}`" = "" ] )
-        then
-                /bin/echo "Couldn't initiate ssh key scan please try again (make sure the machine is online"
-                /bin/rm ${MACHINE_PUBLIC_KEYS}
-                exit
-        fi
-
-		scaling_disabled="`/usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${MACHINE_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USER}@${ip} "/usr/bin/test -f /home/${SERVER_USER}/runtime/scaling/SCALING_DISABLED && /bin/echo 'SCALING_DISABLED'"`" 
-
-		if ( [ "${scaling_disabled}" = "SCALING_DISABLED" ] )
-		then
-			/bin/echo "Scaling  is disabled at the moment I have to bail"
-			exit
-		fi
-done
+/usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${MACHINE_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USER}@${ip} "/usr/bin/test -f /home/${SERVER_USER}/runtime/scaling/SCALING_ENABLED && /bin/echo 'SCALING_ENABLED'"
