d7adfebfecd74cc55ac5b6ae52f3841025cd0049
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 09:05:44 +0100

    Fix syntax errors and enhance SSH key handling
    Corrected syntax errors in the script and improved SSH key handling logic.

--- helpers/scaling/UpdateAutoscalersWithScalingConfig.sh
+++ helpers/scaling/UpdateAutoscalersWithScalingConfig.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 set -x
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
@@ -8,41 +10,29 @@ MACHINE_PUBLIC_KEYS="${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys
 ALGORITHM="`${BUILD_HOME}/helpers/services/GetVariableValue.sh ALGORITHM`"
 SERVER_USER="`/bin/cat ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER`"
 
-
-
 machine_type="autoscaler"
 machine_type_token="as"
+
+ip="${1}"
 
 if ( [ ! -f ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/scaling.conf ] )
 then
         exit
 else
         sourcefile="${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/scaling.conf"
-        /bin/cp ${sourcefile} ${sourcefile)-incoming
+        /bin/cp ${sourcefile} ${sourcefile}-incoming
 fi
 
-token_to_match="${machine_type_token}-`${BUILD_HOME}/helpers/services/GetVariableValue.sh REGION`-${BUILD_IDENTIFIER}"
-
-if ( [ -f ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
+if ( [ ! -f ${MACHINE_PUBLIC_KEYS} ] )
 then
-        ips="`${BUILD_HOME}/services/server/GetServerPrivateIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
-else
-        ips="`${BUILD_HOME}/services/server/GetServerIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
+        /usr/bin/ssh-keyscan  -p ${SSH_PORT} ${ip} > ${MACHINE_PUBLIC_KEYS}
 fi
 
-for ip in ${ips}
-do
-        if ( [ ! -f ${MACHINE_PUBLIC_KEYS} ] )
-        then
-                /usr/bin/ssh-keyscan  -p ${SSH_PORT} ${ip} > ${MACHINE_PUBLIC_KEYS}
-        fi
-
-        if ( [ "`/bin/cat ${MACHINE_PUBLIC_KEYS}`" = "" ] )
-        then
-                /bin/echo "Couldn't initiate ssh key scan please try again (make sure the machine is online"
-                /bin/rm ${MACHINE_PUBLIC_KEYS}
-                exit
-        fi
-        /usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${MACHINE_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USER}@${ip} "/bin/mkdir -p /home/${SERVER_USER}/runtime/scaling" 2>/dev/null
-        /usr/bin/scp -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${MACHINE_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -P ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${sourcefile)-incoming ${SERVER_USER}@${ip}:/home/${SERVER_USER}/runtime/scaling
-done
+if ( [ "`/bin/cat ${MACHINE_PUBLIC_KEYS}`" = "" ] )
+then
+        /bin/echo "Couldn't initiate ssh key scan please try again (make sure the machine is online"
+        /bin/rm ${MACHINE_PUBLIC_KEYS}
+        exit
+fi
+/usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${MACHINE_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USER}@${ip} "/bin/mkdir -p /home/${SERVER_USER}/runtime/scaling" 2>/dev/null
+/usr/bin/scp -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${MACHINE_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -P ${SSH_PORT} -i ${BUILD_HOME}/runtime/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${sourcefile}-incoming ${SERVER_USER}@${ip}:/home/${SERVER_USER}/runtime/scaling
